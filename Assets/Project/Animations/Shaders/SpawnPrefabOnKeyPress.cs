cabcbf6418e94fbc2631eb2a6a1ba6375188877e
Secla_Panda <61236755+SeclaPanda@users.noreply.github.com>
2024-06-07 23:03:20 +0300

    update metod of sonar spawn

--- Assets/Project/Animations/Shaders/SpawnPrefabOnKeyPress.cs
+++ Assets/Project/Animations/Shaders/SpawnPrefabOnKeyPress.cs
@@ -52,18 +52,48 @@ public class SpawnPrefabOnKeyPress : MonoBehaviour
     [Header("Prefab Settings")]
     public GameObject prefabToSpawn; // Префаб для создания
     public Transform spawnPoint; // Место создания префаба
+    public float stepDistance = 1f; // расстояние, которое считается за шаг
+    private int stepCount; // кол-во шагов
+    private Vector3 lastPosition; // позиция игрока в прошлом кадре
+    void Start()
+    {
+        // получаем позицию игрока в прошлом кадре
+        lastPosition = transform.position;
+        SpawnPrefab();
+    }
 
     void Update()
     {
-        // Проверяем нажатие кнопки "B"
-        if (Input.GetKeyDown(KeyCode.B))
+        // текущая позиция игрока
+        Vector3 currentPosition = transform.position;
+
+        // вычисляем расстояние которое прошел игрока относительно предыдущего кадра
+        float distance = Vector3.Distance(currentPosition, lastPosition);
+
+        // если расстояние между текущей и прошлой позицией больше дистанции шага
+        if (distance >= stepDistance)
         {
-            SpawnPrefab();
+            // увеливаем кол-во шагов
+            stepCount++;
+
+            // если кол-во шагов больше или равно n
+            if (stepCount >= 15)
+            {
+                // сбрасываем кол-во шагов
+                stepCount = 0;
+
+                // вызываем метод SpawnPrefab()
+                SpawnPrefab();
+            }
+
+            // получаем текущую позицию игрока в качестве позиции игрока в прошлом кадре
+            lastPosition = currentPosition;
         }
     }
 
     void SpawnPrefab()
     {
+        /*
         // Создаем новый экземпляр префаба в указанной точке
         GameObject newPrefab = Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
 
@@ -73,5 +103,24 @@ public class SpawnPrefabOnKeyPress : MonoBehaviour
             newPrefab.SetActive(true);
         }
         Debug.Log("Всё норм");
+        */
+        GameObject newPrefab = Instantiate(prefabToSpawn);
+
+        // получаем текущую позицию игрока
+        Vector3 position = transform.position;
+
+        // вычисляем новую позицию префаба
+        position.x += 2f; // добавляем 2 к координате x
+
+        // устанавливаем новую позицию и вращение префаба
+        newPrefab.transform.position = position;
+        newPrefab.transform.rotation = Quaternion.identity;
+
+        if (!newPrefab.activeSelf)
+        {
+            newPrefab.SetActive(true);
+        }
+
+        Debug.Log("spawn!");
     }
 }
