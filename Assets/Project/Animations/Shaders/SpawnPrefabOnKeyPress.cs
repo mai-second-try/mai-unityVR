e3d26272661b79cd6b82cf59f22498fc5ae3b617
DanTarasevskiy <116737799+DanTarasevskiy@users.noreply.github.com>
2024-06-08 12:29:30 +0300

    vr simulator finally works

--- Assets/Project/Animations/Shaders/SpawnPrefabOnKeyPress.cs
+++ Assets/Project/Animations/Shaders/SpawnPrefabOnKeyPress.cs
@@ -1,48 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR;
-using UnityEngine.XR.Interaction.Toolkit;
-
-public class SpawnPrefabOnButtonPress : MonoBehaviour
-{
-    [Header("Prefab Settings")]
-    public GameObject prefabToSpawn; // Префаб для создания
-    public Transform spawnPoint; // Место создания префаба
-
-    private XRController xrController;
-
-    void Start()
-    {
-        xrController = GetComponent<XRController>();
-        if (xrController == null)
-        {
-            Debug.LogError("XR Controller not found on this object!");
-        }
-    }
-
-    void Update()
-    {
-        // Проверяем нажатие кнопки "A" на XR контроллере
-        if (xrController.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed) && buttonPressed)
-        {
-            SpawnPrefab();
-        }
-    }
-
-    void SpawnPrefab()
-    {
-        // Создаем новый экземпляр префаба в указанной точке
-        GameObject newPrefab = Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
-
-        // Активируем только в случае, если префаб был неактивным
-        if (!newPrefab.activeSelf)
-        {
-            newPrefab.SetActive(true);
-        }
-    }
-}       */
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -77,7 +32,7 @@ public class SpawnPrefabOnKeyPress : MonoBehaviour
             stepCount++;
 
             // если кол-во шагов больше или равно n
-            if (stepCount >= 15)
+            if (stepCount >= 7)
             {
                 // сбрасываем кол-во шагов
                 stepCount = 0;
